8a698185e2bd40fdc9e45128cc229d1a0c3e18c2
Eslam Genedy <eslamgenedy377@yahoo.com>
2025-04-28 12:35:38 +0300

    initial commit

---
+++ src/main/java/com/example/modular_booking_system/ModularBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.modular_booking_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModularBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModularBookingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/modular_booking_system/ModularBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.modular_booking_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModularBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
